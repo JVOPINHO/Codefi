416460578f1671500291e2ee8ecfbc641bef0c9e
JVOPINHO <joaoamoreterno1511@gmail.com>
2021-06-29 00:32:30 -0300

    adicionado comando 'play'

--- LavalinkManager.js
+++ LavalinkManager.js
@@ -1,5 +1,5 @@
-const { Manager } = require("erela.js")
-const Discord = require("discord.js")
+const { Manager } = require("erela.js");
+const Discord = require("discord.js");
 
 module.exports = (client) => {
     client.music = new Manager({
@@ -18,27 +18,24 @@ module.exports = (client) => {
             if (guild) guild.shard.send(payload);
         }
     })
-
     .on("nodeConnect", node => console.log(`[LAVALINK]: O node '${node.options.identifier}' foi conectado a lavalink!`))
     .on("nodeError", (node, error) => console.log(`[LAVALINK]: O node '${node.options.identifier}' teve um erro ao conectar na lavalink: ${error.message}`))
     .on("playerMove", (player, currentChannel, newChannel) => {
         player.voiceChannel = client.channels.cache.get(newChannel);
     })
     .on("trackStart", async (player, track) => {
-        const channel = client.channels.cache.get(player.textChannel)
+        const channel = client.channels.cache.get(player.textChannel);
 
         channel.send(new Discord.MessageEmbed()
             .setColor("#B0E0E6")
             .setDescription(`:musical_note: Iniciando o lofi`)
-            .setThumbnail(track.displayThumbnail())
-        )
+            .setThumbnail(track.displayThumbnail()));
     })
     .on("queueEnd", async (player) => {
-        const channel = client.channels.cache.get(player.textChannel)
+        const channel = client.channels.cache.get(player.textChannel);
 
         channel.send(new Discord.MessageEmbed()
             .setColor("#EE82EE")
-            .setDescription(`Eita, o lofi acabou :cry:`)
-        )
-    })
-}
+            .setDescription(`Eita, o lofi acabou :cry:`));
+    });
+};
--- index.js
+++ index.js
@@ -9,17 +9,36 @@ const client = new Discord.Client({
   partials: ["MESSAGE", "CHANNEL", "REACTION"]
 });
 
-require("./LavalinkManager")(client)
+require("./LavalinkManager")(client);
 
 client.on("ready", async () => {
-  console.log(`[CLIENT]: O client do bot fez login no Discord com sucesso!`)
-  client.music.init(client.user.id)
+  console.log(`[CLIENT]: O client do bot fez login no Discord com sucesso!`);
+  client.music.init(client.user.id);
 });
 
 client.on("message", async (message) => {
   if (message.content.toLocaleLowerCase() === "<3play") {
-    //Terminar aqui
-  }
+    let checkNode = client.music.nodes.get("Cod 1");
+    if(!checkNode) return message.channel.send(":x: **| Estou sem conecxão com minha instancia lavalink! Vote mais tarde, ok?**");
+    let player = client.music.players.get(message.guild.id);
+    if(player) return message.channel.send(`:octagonal_sign: **| Eu já tenho um plyaer ativo!**\n> Player ativo em: <#${player.voiceChannel}>`);
+    if(message.member.voice.channel.id != process.env.channelId) return message.channel.send(`:octagonal_sign: **| Que tal irmos para o canal <#${process.env.channelId}>?**`);
+
+    player = client.music.create({
+      guild: message.guild.id,
+      voiceChannel: message.member.voice.channel.id,
+      textChannel: message.channel.id,
+      selfDeafen: true
+    });
+
+    player.connect();
+
+    let results = await player.search(process.env.url, message.author);
+
+    if(["NO_MATCHES", "LOAD_FAILED"].includes(results.loadType)) return message.channel.send(":x: **| Desculpa, aconteceu um erro ao carregar a música!**");
+    player.queue.add(results.tracks[0]);
+    player.play();
+  };
 });
 
 client.login(process.env.token);
